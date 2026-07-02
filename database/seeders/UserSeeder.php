f00756e72a8e4c4bc825d7a8ddcbc6ce182fa7fa
Sandy <sandylima.st@gmail.com>
2026-07-02 08:42:40 -0300

    Adiciona usuários de teste via UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,37 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        //Neste user se encontram itens colecionáveis adicionados e fotos 
+        User::firstOrCreate(
+            ['email' => 'sandy@teste.com'],
+            [
+                'name' => 'Sandy',
+                'password' => Hash::make('teste1234'),
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'admin@vault.com'],
+            [
+                'name' => 'Administrador',
+                'password' => Hash::make('12345678'),
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'user@vault.com'],
+            [
+                'name' => 'Usuário',
+                'password' => Hash::make('12345678'),
+            ]
+        );
     }
 }
